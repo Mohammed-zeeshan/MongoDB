e39bf82474479a2c6d693a18bccc311417508e60
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2024-07-12 00:29:33 +0530

    Relations between Tables

--- user.js
+++ user.js
@@ -4,9 +4,11 @@ const getDb = require("../util/database");
 const objectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
@@ -14,13 +16,29 @@ class User {
     db.collection("user").insertOne(this);
   }
 
+  addToCart(product) {
+    // const cartProducts = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb;
+    db.collection("user").updateOne(
+      { _id: new objectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
+  }
+
   static findById(userId) {
     const db = getDb();
-    return db.collection("user").findOne({ _id: new objectId(userId) }).then((user => {
-      console.log(user);
-    })).catch((err) => {
-      console.log(err);
-    });
+    return db
+      .collection("user")
+      .findOne({ _id: new objectId(userId) })
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
